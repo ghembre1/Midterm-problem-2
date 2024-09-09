8392cafe9e1f1f0d3994619d1efb35cc90288fd4
Gary Hembree <gbhembree22@amarillocollege.com>
2024-09-09 14:44:17 -0500

    Add project files.

---
+++ Midterm problem 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm_problem_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number of units consumed"); //prompt user to enter units
+            double units = Convert.ToDouble(Console.ReadLine()); // makes users input a variable
+
+            if (units > 500)
+            {
+                
+            }
+            if (units > 300)
+            {
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Total bill is " ); // prints the total bill
+            Console.Read();
+        }
+    }
+}
